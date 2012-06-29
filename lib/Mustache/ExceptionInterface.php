18cd5d955707a9dbc9f4b421f6a6efabe9ef3cb9
adlawson <adlawson@gmail.com>
2012-06-29 19:42:47 +0100

    Added exception interface and lexer exception

---
+++ lib/Mustache/ExceptionInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace Mustache;
+
+/**
+ * Provides a nice way to catch all exceptions
+ * thrown by Mustache
+ * 
+ * @package  Mustache
+ * @license  MIT License <LICENSE>
+ * @link     http://github.com/adlawson/mustache
+ */
+interface ExceptionInterface
+{
+
+}
